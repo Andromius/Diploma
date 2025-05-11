f26c9f3916b665e6a901784d091a6eb731da3bb3
Andromius <miniswiczmartin@gmail.com>
2025-05-11 19:34:47 +0200

    Various updates

--- tests/test_unit/test_grayscaling.py
+++ tests/test_unit/test_grayscaling.py
@@ -1,6 +1,6 @@
 from filters.preprocessing.grayscaling import GrayscaleFilter
 
-def test_grayscaling_conversion(test_image,app):
+def test_grayscaling_conversion(test_image_dict,app):
     filter = GrayscaleFilter(app.logger)
-    data = filter.apply(test_image)
+    data = filter.apply(test_image_dict)
     assert len(data['grayscale'].shape) == 2, "Image is not grayscaled."
--- tests/test_unit/test_model_filter.py
+++ tests/test_unit/test_model_filter.py
@@ -6,18 +6,18 @@ import torch
 
 class TestModelFilter(TestFilter):
 
-    # def test_yolo_apply(self, test_yolo_image, app):
+    # def test_yolo_apply(self, test_yolo_image_dict, app):
     #     # Assuming the model has a method called 'predict' that takes an image and returns predictions
     #     filter = ModelSelectionFilter("yolo", app.logger)
-    #     predictions = filter.apply(test_yolo_image)
+    #     predictions = filter.apply(test_yolo_image_dict)
     #     assert predictions is not None, "Model selection filter did not return predictions."
     #     assert isinstance(predictions, list), "Predictions should be a list."
 
-    def test_maskrcnn_apply(self, test_image, maskrcnn_model : MaskRCNNSegmentationFilter):
+    def test_maskrcnn_apply(self, test_image_dict, maskrcnn_model : MaskRCNNSegmentationFilter):
         # Assuming the model has a method called 'predict' that takes an image and returns predictions
         print(torch.version.cuda)
         print(torch.cuda.is_available())
-        data = maskrcnn_model.apply(test_image)
+        data = maskrcnn_model.apply(test_image_dict)
         assert data['segmentation_data'] is not None, "Model selection filter did not return predictions."
         assert isinstance(data['segmentation_data'], list), "Predictions should be a list."
     
